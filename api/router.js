a6f1cd5cfa214439700d7bc0a0514b12dea7bdde
Denis Tkachuk <deventorum@gmail.com>
2020-01-31 15:19:29 -0500

    Updated Readme.md, minor changes

--- api/router.js
+++ api/router.js
@@ -1,9 +1,10 @@
 const express = require('express');
+
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    console.log('received request');
-    res.json({message: 'Success'});
-})
+  console.log('received request');
+  res.json({ message: 'Success' });
+});
 
 module.exports = router;
