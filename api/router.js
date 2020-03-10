fb7044f9e8eb3b10b7f30a4bc5f247938dbdeda7
Denis Tkachuk <deventorum@gmail.com>
2020-03-10 17:04:13 -0400

    Fixed proxy setup in the local environment to handle large files

--- api/router.js
+++ api/router.js
@@ -3,7 +3,8 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/upload', (req, res) => {
-  console.log(req);
+  // console.log(req);
+  console.log('Received file');
   res.json({ message: 'Upload' });
 });
 
