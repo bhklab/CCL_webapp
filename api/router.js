afd71848b5d9daa1bbb24c2173bb8d12cb445781
Denis Tkachuk <deventorum@gmail.com>
2020-03-11 16:42:04 -0400

    Cleaned up file upload code + deleted useless dependencies

--- api/router.js
+++ api/router.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const multer = require('multer');
 const vcf = require('bionode-vcf');
-const path = require('path');
-const fs = require('fs');
 const { Readable } = require('stream');
 
 
@@ -25,65 +23,19 @@ const upload = multer({ storage: multer.memoryStorage() });
 const router = express.Router();
 
 router.post('/upload', upload.single('file'), (req, res) => {
-  // console.log(req);
   console.log('Received file');
-  // console.log(String(req.file.buffer));
-
-  // const fileStrem .createReadStream();
-  const fileStream = fs.createReadStream(path.join(__dirname, '../test data/a549.sample_id copy.vcf'));
-  // console.log(fileStream);
-  // vcf.readStream(bufferToStream(req.file.buffer))
-  vcf.readStream(fileStream)
+  const requestObj = [];
+  vcf.readStream(bufferToStream(req.file.buffer))
     .on('data', (feature) => {
-      // console.log(feature);
+      requestObj.push(feature);
     }).on('error', (err) => {
       console.error('it\'s not a vcf', err);
     })
-    .once('end', console.log('finished'));
-
-  console.log('here');
-
-  // fs.readFile(path.join(__dirname, '../test data/a549.sample_id copy.vcf'), (err, data) => {
-  //   if (err) throw err;
-  //   console.log(req.file.buffer);
-
-  //   vcf.readStream(data);
-  //   // vcf.read(path.join(__dirname, '../test data/a549.sample_id copy.vcf'));
-  //   vcf.on('data', (feature) => {
-  //     console.log(feature);
-  //   });
-
-  //   vcf.on('end', () => {
-  //     console.log('end of file');
-  //   });
-
-  //   vcf.on('error', (err) => {
-  //     // console.error('it\'s not a vcf', err);
-  //   });
-  // });
-
-  // vcf.readStream(String(req.file.buffer), 'vcf');
-  // // vcf.read(path.join(__dirname, '../test data/a549.sample_id copy.vcf'));
-  // vcf.on('data', (feature) => {
-  //   // console.log(feature);
-  // });
-
-  // vcf.on('end', () => {
-  //   console.log('end of file');
-  // });
-
-  // vcf.on('error', (err) => {
-  //   // console.error('it\'s not a vcf', err);
-  // });
-
-  // vcard.parseVcardFile(path.join(__dirname, '../test data/a549.sample_id copy.vcf'), (err, data) => {
-  //   if (err) console.log(`oops:${err}`);
-  //   else {
-  //     console.log(`should be good to go:\n${JSON.stringify(data)}`);
-  //   }
-  // });
-
-  res.json({ message: 'Upload' });
+    .once('end', () => {
+      res.json({ message: 'Upload' });
+      console.log(requestObj[0]);
+      console.log('finished');
+    });
 });
 
 router.get('/', (req, res) => {
