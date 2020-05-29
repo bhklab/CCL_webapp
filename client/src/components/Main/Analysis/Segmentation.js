574e81db08f6604e7d80ec2054357596bd816cd0
deventorum <deventorum@gmail.com>
2020-05-29 13:14:56 -0400

    Segmentation data visualization + cleaning R output

--- client/src/components/Main/Analysis/Segmentation.js
+++ client/src/components/Main/Analysis/Segmentation.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
-import colors from '../../../styles/colors'
+import 'react-table-6/react-table.css';
+import ReactTable from 'react-table-6';
+
+import colors from '../../../styles/colors';
+import standardizeROutput from '../../utils/standardizeROutput'
 
 const StyledSegmentation = styled.div`
   margin: 10px;
@@ -11,11 +15,67 @@ const StyledSegmentation = styled.div`
   }
 `;
 
-function Segmentation(props) {
+const columns = [
+  {
+    Header: 'ID',
+    accessor: 'ID',
+  },
+  {
+    Header: 'Arm',
+    accessor: 'arm',
+  },
+  {
+    Header: 'Chromosome',
+    accessor: 'chrom',
+  },
+  {
+    Header: 'Locus Start',
+    accessor: 'locstart',
+  },
+  {
+    Header: 'Locus End',
+    accessor: 'locend',
+  },
+  {
+    Header: 'num.mark',
+    accessor: 'nummark',
+  },
+  {
+    Header: 'seg.diff',
+    accessor: 'segdiff',
+  },
+  {
+    Header: 'seg.mean',
+    accessor: 'segmean',
+  },
+  {
+    Header: 'sed.sd',
+    accessor: 'segsd',
+  },
+  {
+    Header: 'seg.z',
+    accessor: 'segz',
+  },
+  {
+    Header: 't',
+    accessor: 't',
+  },
 
+]
+
+function Segmentation(props) {
+  const { data } = props;
+  console.log(data);
+  const standardizedData = standardizeROutput(data);
+  console.log(standardizedData)
   return (
     <StyledSegmentation>
       <h3>Segmentation</h3>
+      <ReactTable
+        columns={columns}
+        data={standardizedData}
+        defaultPageSize={10}
+      />
     </StyledSegmentation>
   )
 }
