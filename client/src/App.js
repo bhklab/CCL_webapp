29c7ab20b2f5572d0dfccdf6317b19f9a7f6506e
deventorum <deventorum@gmail.com>
2020-07-17 17:52:10 -0400

    Google Analytics

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Normalize } from 'styled-normalize';
 import GlobalStyles from './styles/GlobalStyles';
+import ReactGA from 'react-ga';
 
 import Home from './components/Main/Home';
 import TopNav from './components/TopNav/TopNav';
 
-function App() {
+const App = () => {
+    // Google analytics setup.
+    useEffect(() => {
+        ReactGA.initialize('UA-102362625-8');
+        // To Report Page View
+        ReactGA.pageview(window.location.pathname + window.location.search);
+    }, []);
+
     return (
         <div className="App">
             <Normalize />
